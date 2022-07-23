06805ad77ebf4c49bf83887cfa2bc22001bf8d12
C. James <33495307+Arrowkill@users.noreply.github.com>
2022-07-23 02:38:12 -0500

    Finished basic navbar look
    Finished making navbar's looks but lacks search functionality and links.

--- enragedtabbycinema/components/Navbar.jsx
+++ enragedtabbycinema/components/Navbar.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
-import {useState} from 'react'
-{/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'*/}
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+
+  {/* This section adds FontAwesome icons to be used */}
+  library.add(
+    faMagnifyingGlass,
+    faXmark,
+  )
+
   {/* CSS Style Constants */}
+  {/* This block of CSS handles the navbar as a whole 
+    * and ensures it is formatted with the proper font and alignment */}
   const navbar= {
+    display: 'flex',
+    whiteSpace: 'nowrap',
     outline: 'none',
     boxSizing: 'border-box',
     fontFamily: 'Trebuchet MS',
     backgroundColor: '#171717',
-    display: 'flex',
+    backgroundSize: 'cover',
     alignItems: 'center',
     justifyContent: 'space-between',
-    height: '50px',
+    lineHeight: '40px',
     padding: '0px 100px'
   }
+
+  {/* This block of CSS handles the logo of Enraged Tabby on the navbar */}
   const logo = {
     color: 'white',
     fontSize: '30px',
     fontWeight: '600',
     letterSpacing: '-1px'
   }
+
+  {/* This block of CSS handles the navbar options in the center. 
+    * This includes formatting the options collectively and individually. 
+    * This also includes the hooks for allowing you to hover individually on each link
+    * and create a hover effect. */}
   const navbar_option = {
     display: 'flex',
     flex: '1',
-    padding: '0px 0px 0px 350px'
+    padding: '0px 0px 0px 325px'
   }
   const navbar_option_li = {
     listStyle: 'none',
@@ -42,7 +62,7 @@ const Navbar = () => {
     fontSize: '20px',
     fontWeight: '500',
     textDecoration: 'none',
-    transition: '1s'
+    transition: '700ms'
   }
   const [isHoveringShowtimes, setIsHoveringShowtimes] = useState(false);
   const onMouseMoveOnShowtimes = () => {
@@ -56,7 +76,7 @@ const Navbar = () => {
     fontSize: '20px',
     fontWeight: '500',
     textDecoration: 'none',
-    transition: '1s'
+    transition: '700ms'
   }
   const [isHoveringOurCinema, setIsHoveringOurCinema] = useState(false);
   const onMouseMoveOnOurCinema = () => {
@@ -65,12 +85,112 @@ const Navbar = () => {
   const onMouseMoveOffOurCinema = () => {
     setIsHoveringOurCinema(false);
   }
-  const navbar_option_li_a_our = {
+  const navbar_option_li_a_ourCinema = {
     color: isHoveringOurCinema ? '#00a8e1' : '#e7e9eb',
     fontSize: '20px',
     fontWeight: '500',
     textDecoration: 'none',
-    transition: '1s'
+    transition: '700ms'
+  }
+
+  {/* This block of CSS handles the searchbar element of the navbar.
+    * This includes the fly-in and fly-out as well as the icon switch for the 
+    * searchbar. */}
+  const [isClickedSearch, setIsClickedSearch] = useState(true);
+  const onMouseClickSearch = () => {
+    setIsClickedSearch(!isClickedSearch);
+  }
+  const searchbar = {
+    position: 'relative',
+    webkitUserSelect: 'none',
+    mozUserSelect: 'none',
+    msUserSelect: 'none'
+  }
+  const searchbar_input = {
+    marginRight: '40px',
+    border: '0px',
+    padding: isClickedSearch ? '0px' : '0px 10px',
+    width: isClickedSearch ? '0px' : '250px',
+    height: '35px',
+    borderRadius: '3px',
+    transition: isClickedSearch ? 'all 0.3s ease' : 'all 0.5s 0.2 ease'
+  }
+  const searchbar_button = {
+    display: 'flex',
+    position: 'absolute',
+    top: '0px',
+    right: '0px',
+    width: '40px',
+    height: '100%',
+    background: 'none',
+    color: '#fff',
+    transition: 'all 0.5s 0.3s ease'
+  }
+  const searchbar_button_magnifying = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    opacity: isClickedSearch ? '1' : '0',
+    cursor: 'pointer',
+    transition: '700ms'
+  }
+  const searchbar_button_x = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    opacity: isClickedSearch ? '0' : '1',
+    cursor: 'pointer',
+    transition: '700ms'
+  }
+
+  {/* This CSS block creates the divider between the search and sign-in/register 
+    * section on the navbar */}
+    const divider = {
+      borderLeft: '.5px solid white',
+      height: '30px',
+      marginLeft: '10px'
+    }
+
+  {/* This block of CSS handles the sign-in and register links.
+    * This section also includes the hooks that allow the hover effect to work. */}
+  const [isHoveringSignIn, setIsHoveringSignIn] = useState(false);
+  const onMouseMoveOnSignIn = () => {
+    setIsHoveringSignIn(true);
+  }
+  const onMouseMoveOffSignIn = () => {
+    setIsHoveringSignIn(false);
+  }
+  const login_signIn = {
+    color: isHoveringSignIn ? '#00a8e1' : '#e7e9eb',
+    fontSize: '20px',
+    fontWeight: '500',
+    textDecoration: 'none',
+    transition: '700ms'
+  }
+  const login_or = {
+    color: '#e7e9eb',
+    fontSize: '18px',
+    fontWeight: '400',
+    textDecoration: 'none',
+    marginLeft: '2px',
+    marginRight: '2px'
+    
+  }
+  const [isHoveringRegister, setIsHoveringRegister] = useState(false);
+  const onMouseMoveOnRegister = () => {
+    setIsHoveringRegister(true);
+  }
+  const onMouseMoveOffRegister = () => {
+    setIsHoveringRegister(false);
+  }
+  const login_register = {
+    color: isHoveringRegister ? '#00a8e1' : '#e7e9eb',
+    fontSize: '20px',
+    fontWeight: '500',
+    textDecoration: 'none',
+    transition: '700ms'
   }
 
   {/* HTML Tags in React */}
@@ -91,21 +211,25 @@ const Navbar = () => {
           </a>
         </li>
         <li style={navbar_option_li}>
-          <a style={navbar_option_li_a_our} onMouseEnter={onMouseMoveOnOurCinema} onMouseLeave={onMouseMoveOffOurCinema} href='#!'>
+          <a style={navbar_option_li_a_ourCinema} onMouseEnter={onMouseMoveOnOurCinema} onMouseLeave={onMouseMoveOffOurCinema} href='#!'>
             Our Cinema
           </a>
         </li>
       </div>
-      <div class='searchbar'>
-        <input type='text' placeholder='Search'>
-        </input>
-        {/*<FontAwesome icon='fas fa-search' />*/}
+      <div style={searchbar} class='searchbar' unselectable='off'>
+        <input style={searchbar_input} type='text' placeholder='Search' />
+        <div style={searchbar_button} class='searchbar-button'>
+          <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" style={searchbar_button_magnifying} onClick={onMouseClickSearch} disabled={isClickedSearch} />
+          <FontAwesomeIcon icon="fa-solid fa-xmark" style={searchbar_button_x} onClick={onMouseClickSearch} disabled={!isClickedSearch} />
+        </div>
       </div>
-      <div>
-        |
-      </div>
-      <div>
-        <li style={navbar_option_li}>Sign In or Create an Account</li>
+      <div style={divider} class='divider' />
+      <div style={{whiteSpace: 'nowrap'}}>
+        <li style={navbar_option_li}>
+          <a style={login_signIn} onMouseEnter={onMouseMoveOnSignIn} onMouseLeave={onMouseMoveOffSignIn} href='#!'>Sign In</a>
+          <a style={login_or}> or </a>
+          <a style={login_register} onMouseEnter={onMouseMoveOnRegister} onMouseLeave={onMouseMoveOffRegister} href='#!'>Create an Account</a>
+        </li>
       </div>
     </div>
   )
