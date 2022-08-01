704f483bc1d868d00c0738b4b029cfdbb7c3ee5f
C. James <33495307+Arrowkill@users.noreply.github.com>
2022-07-31 20:36:49 -0500

    changes for nextjs gh-pages

--- enragedtabbycinema/next.config.js
+++ enragedtabbycinema/next.config.js
@@ -4,4 +4,8 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const isProd = process.send.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/development-react/' : ''
+}
