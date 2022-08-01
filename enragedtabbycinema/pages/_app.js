e5e3c0884d5c5d308c87af18ca901e2311fab0e7
zehgoo <samnjogo@outlook.com>
2022-07-31 23:02:09 -0500

    functionality for movies page

--- enragedtabbycinema/pages/_app.js
+++ enragedtabbycinema/pages/_app.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import '../styles/test.css'
-=======
 import '../styles/globals.css'
->>>>>>> 78eee6f7bcd827880fe0a5a59673840a87002eed
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyAps
